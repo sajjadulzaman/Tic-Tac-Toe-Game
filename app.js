d8c78d7863793895af758b9d3c38711011956e7b
sajjaduz zaman <sajjadulzaman@gmail.com>
2025-12-12 00:14:46 +0600

    button function has been add

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-const boxs = document.querySelectorAll".box";
+const boxs = document.querySelectorAll(".box")
 const restBtn = document.getElementById('Reset-btn') 
+let trunO = true;
+// 2d array
+const winPartterns= [
+  [0,1,2],
+  [0,3,6],
+  [0,4,8],
+  [1,4,7],
+  [2,5,8],
+  [2,4,6],
+  [3,4,5],
+  [6,7,8],
+]
+boxs.forEach((box) => {
+  box.addEventListener('click', () => {
+    console.log('button has clicked ');
+    if (trunO) {
+      box.innerText = 'O';
+      trunO = false;
+    }
+    else {
+      box.innerText = 'X';
+      trunO = true;
+    }
+  })
+})
